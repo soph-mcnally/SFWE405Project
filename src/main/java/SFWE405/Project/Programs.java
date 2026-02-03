d84225c14b89c2747eabab02fa31636241c3cf12
jeriah30 <jeriahjtg@arizona.edu>
2026-02-03 15:17:02 -0700

    programs class creation

--- src/main/java/SFWE405/Project/Programs.java
+++ src/main/java/SFWE405/Project/Programs.java
@@ -1,4 +1,15 @@
 package SFWE405.Project;
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+@Entity
 public class Programs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String programName;
+    private double creditsRequired;
 }
