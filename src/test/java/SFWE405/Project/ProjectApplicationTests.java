33bc10b61212069596a4f2766db8b67161d35e76
soph-mcnally <105947318+soph-mcnally@users.noreply.github.com>
2026-02-03 14:30:41 -0700

    Initial commit

---
+++ src/test/java/SFWE405/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SFWE405.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
