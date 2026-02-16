727b668fe075ee95fe6832193b76dcc06e3e7386
Brandon Sisco <166445283+Brhando@users.noreply.github.com>
2026-02-15 19:16:03 -0700

    Update People.java
    cleaned up the code; added annotations, deleted getters and setters (included in @Data), imported needed libraries, etc.

--- src/main/java/SFWE405/Project/model/People.java
+++ src/main/java/SFWE405/Project/model/People.java
@@ -6,18 +6,20 @@ import java.util.HashSet;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data //<- is this annotation needed?
 @Entity
 public class People {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String firstName;
     private String lastName;
     
-    @Cloumn(unique = true) //each person should have a unique email -> no repeating emails
+    @Column(unique = true, nullable = false) //each person should have a unique email -> no repeating emails
     private String email;
 
     /* adding the relationship here for a join table, but I'm still uncertain how we are doing this
@@ -27,9 +29,12 @@ public class People {
             joinColumns = @JoinColumn(name = "") <- need to figure out what the FK is we are joining here
             inverseJoinColumn = @JoinColumn(name = "") <- same as above
     */
-    private Set<Courses> courses = new HashSet<>();
+    // private Set<Courses> courses = new HashSet<>();
 
     @ManyToOne (optional = false) //this side owns this relationship, as it's the side with the multiplicity
+    @JoinColumn (name = "university_id", nullable = false)
+    @ToString.Exclude // excluding these from the @Data annotation to avoid recursion and equality bugs
+    @EqualsAndHashCode.Exclude
     private Universities enrolledAt;
 
     public enum PersonType { // <- we should consider moving these into their own file
@@ -37,43 +42,36 @@ public class People {
         FACULTY,
         ADMIN
     }
+    @Enumerated(EnumType.STRING)
     private PersonType personType; 
 
     public enum DegreeLevel {
         UNDERGRADUATE,
         GRADUATE
     }
+    @Enumerated(EnumType.STRING)
     private DegreeLevel degreeLevel;
 
     //constructors
     public People() {}
 
-    public People(String firstName, String lastName, String email) {
-        this.firstName = firstname;
+    public People(String firstName, String lastName, String email, PersonType personType, DegreeLevel degreeLevel) {
+        this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.personType = personType;
+        this.degreeLevel = degreeLevel;
     }
 
-    // getters/setters
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String name) { firstName = name; }
-
-    public String getLastName() { return lastName;}
-    public void setLastName(String name) { lastName = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public DegreeLevel getDegreeLevel() { return degreeLevel; }
-    public void setDegreeLevel(DegreeLevel degreeLevel) { this.degreeLevel = degreeLevel; } //not sure if I need to typecast the parameter...
-
-    public PersonType getPersonType() { return personType; }
-    public void setPersonType(PersonType type) { personType = type; } // same question as above
-
-    public Universities getEnrolledAt() { return enrolledAt; }
-    public void setEnrolledAt(Universities university) { // need to update both sides of the database for the relationships described
-        enrolledAt = university;
-        university.enrollPerson(this);
+    // helper to maintain both sides in the database
+    public void setEnrolledAt(Universities newUni) {
+        if (this.enrolledAt != null) {
+            this.enrolledAt.getEnrolled().remove(this);
+        }
+        this.enrolledAt = newUni;
+        if (newUni != null) {
+            newUni.getEnrolled().add(this);
+        }
     }
         
 }
