f7910d630078d72496acaae2db4d8594876e06bb
Brandon Sisco <166445283+Brhando@users.noreply.github.com>
2026-02-15 18:48:32 -0700

    Fleshed out the beginnings of Universities
    Added relationships, getter/setters, and constructors. Additionally added a helper function for adding people to the  "enrolled" relationship (Set).

--- src/main/java/SFWE405/Project/model/Universities.java
+++ src/main/java/SFWE405/Project/model/Universities.java
@@ -2,6 +2,8 @@ package SFWE405.Project.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.util.Set;
+import java.util.HashSet;
 
 @Data
 @Entity
@@ -12,4 +14,29 @@ public class Universities {
 
     private String name;
     private String location;
+
+    @OneToMany (mappedBy = "enrolledAt"); // Peopl owns this relationship -> mappedBy is needed
+    private Set<People> enrolled = new HashSet<>();
+
+    //constructors
+    public Universities() {}
+
+    public Universities(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    // getters/setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public Set<People> getEnrolled() { return enrolled; }
+
+    // helpers
+    public void enrollPerson(People p) {
+        enrolled.add(p);
+    }
 }
