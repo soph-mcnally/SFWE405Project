d0d114c15117b7d32717cd6ad4ec8796a8d1a857
Brandon Sisco <166445283+Brhando@users.noreply.github.com>
2026-02-15 18:46:21 -0700

    Fleshed out the beginnings of People
    Added a few relationships, getters/setters, and constructor functions. Additionally, added a few fields for the relationships.

--- src/main/java/SFWE405/Project/model/People.java
+++ src/main/java/SFWE405/Project/model/People.java
@@ -1,9 +1,13 @@
 package SFWE405.Project.model;
+
+import java.util.Set;
+import java.util.HashSet;
+
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data
+@Data //<- is this annotation needed?
 @Entity
 public class People {
     @Id
@@ -12,19 +16,64 @@ public class People {
 
     private String firstName;
     private String lastName;
+    
+    @Cloumn(unique = true) //each person should have a unique email -> no repeating emails
     private String email;
 
-    public enum PersonType {
+    /* adding the relationship here for a join table, but I'm still uncertain how we are doing this
+    @ManyToMany
+    @JoinTable(
+            name = "people_courses",
+            joinColumns = @JoinColumn(name = "") <- need to figure out what the FK is we are joining here
+            inverseJoinColumn = @JoinColumn(name = "") <- same as above
+    */
+    private Set<Courses> courses = new HashSet<>();
+
+    @ManyToOne (optional = false) //this side owns this relationship, as it's the side with the multiplicity
+    private Universities enrolledAt;
+
+    public enum PersonType { // <- we should consider moving these into their own file
         STUDENT,
         FACULTY,
         ADMIN
     }
-    private PersonType personType;
+    private PersonType personType; 
 
-    public enum degreeLevel {
+    public enum DegreeLevel {
         UNDERGRADUATE,
         GRADUATE
     }
-    private degreeLevel degreeLevel;
+    private DegreeLevel degreeLevel;
 
+    //constructors
+    public People() {}
+
+    public People(String firstName, String lastName, String email) {
+        this.firstName = firstname;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // getters/setters
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String name) { firstName = name; }
+
+    public String getLastName() { return lastName;}
+    public void setLastName(String name) { lastName = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public DegreeLevel getDegreeLevel() { return degreeLevel; }
+    public void setDegreeLevel(DegreeLevel degreeLevel) { this.degreeLevel = degreeLevel; } //not sure if I need to typecast the parameter...
+
+    public PersonType getPersonType() { return personType; }
+    public void setPersonType(PersonType type) { personType = type; } // same question as above
+
+    public Universities getEnrolledAt() { return enrolledAt; }
+    public void setEnrolledAt(Universities university) { // need to update both sides of the database for the relationships described
+        enrolledAt = university;
+        university.enrollPerson(this);
+    }
+        
 }
