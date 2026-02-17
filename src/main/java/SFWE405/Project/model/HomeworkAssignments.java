09dab1005b28c39948a5a56ce705f066ced353a1
travispotterAZ <travispotter@arizona.edu>
2026-02-17 10:17:09 -0700

    Add HomeworkAssignment entity with ManyToOne realtionship to a course

---
+++ src/main/java/SFWE405/Project/model/HomeworkAssignments.java
@@ -0,0 +1,27 @@
+package SFWE405.Project.model;
+
+import SFWE405.Project.model.Courses;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*; //validating attributes
+import java.time.LocalDate;
+
+import lombok.Data;                 //Lombok getters & setters
+
+@SuppressWarnings("unused") //gets rid of unused import warnings
+@Data
+@Entity
+public class HomeworkAssignments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull(message = "Assignment name cannot be null")
+    private String assignmentName;
+    LocalDate dueDate;
+    private String relatedFileName = null;  //allows for accessing files possibly needed (Likely stored in templates)
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false) //FK column in join table references the Courses entity
+    private Courses course;
+}
