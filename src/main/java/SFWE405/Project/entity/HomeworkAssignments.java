666345ead6f7882504e065e6f01a6995e049b59c
Travis Potter <travispotter@arizona.edu>
2026-02-21 14:20:13 -0700

    Rename package from model to entity for HomeworkAssignments

--- src/main/java/SFWE405/Project/entity/HomeworkAssignments.java
+++ src/main/java/SFWE405/Project/entity/HomeworkAssignments.java
@@ -1,6 +1,6 @@
-package SFWE405.Project.model;
+package SFWE405.Project.entity;
 
-import SFWE405.Project.model.Courses;
+import SFWE405.Project.entity.Courses;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*; //validating attributes
