8b83145195e61970b0d845c33e79605926867f0a
soph-mcnally <105947318+soph-mcnally@users.noreply.github.com>
2026-02-21 09:32:33 -0700

    Fixed * for jakarta imports

--- src/main/java/SFWE405/Project/entity/Semesters.java
+++ src/main/java/SFWE405/Project/entity/Semesters.java
@@ -1,6 +1,13 @@
 package SFWE405.Project.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GenerationType;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
