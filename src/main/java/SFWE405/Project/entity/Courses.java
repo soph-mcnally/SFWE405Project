f686dd4ce11f5109a8f6e02561bd6a40f3e4d3c7
jeriah30 <jeriahjtg@arizona.edu>
2026-02-21 13:53:30 -0700

    Fixed Courses import *, but added imports that are missing in the dev.

--- src/main/java/SFWE405/Project/entity/Courses.java
+++ src/main/java/SFWE405/Project/entity/Courses.java
@@ -1,7 +1,17 @@
 package SFWE405.Project.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.FetchType;
 
 @Data
 @Entity
