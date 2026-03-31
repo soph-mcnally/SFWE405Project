e6d2d97d0d374232945223c3845998acec2da664
jeriah30 <166445109+jeriah30@users.noreply.github.com>
2026-03-30 20:58:05 -0700

    Delete src/main/java/SFWE405/Project/repository directory to update new

--- src/main/java/SFWE405/Project/repository/PeopleRepository.java
+++
@@ -1,7 +0,0 @@
-package SFWE405.Project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import SFWE405.Project.entity.People;
-
-public interface PeopleRepository extends JpaRepository<People, Long> {}
--- src/main/java/SFWE405/Project/repository/ProgramsRepository.java
+++
@@ -1,7 +0,0 @@
-package SFWE405.Project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import SFWE405.Project.entity.Programs;
-
-public interface ProgramsRepository extends JpaRepository<Programs, Long> {}
