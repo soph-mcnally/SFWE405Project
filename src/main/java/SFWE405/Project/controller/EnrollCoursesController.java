ada97520adda2a21ef73c96b09611f7226d2d18e
jeriah30 <jeriahjtg@arizona.edu>
2026-04-23 10:59:47 -0700

    Added Enroll Courses page with UI polish and backend integration

--- src/main/java/SFWE405/Project/controller/EnrollCoursesController.java
+++ src/main/java/SFWE405/Project/controller/EnrollCoursesController.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/enrollment")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EnrollCoursesController {
 
     private final AuthenticationService authenticationService;
